fa50b934b994d989821dcd9e8f13844e3a82b689
designerasun <asuntheawesome@gmail.com>
2022-07-17 00:23:50 +0900

    ENH:parsing bytes32 to string with ethersjs

--- dependencies/ethersJs/format.js
+++ dependencies/ethersJs/format.js
@@ -1,4 +1,3 @@
-import 'ethers'
 import { BigNumber, ethers } from 'ethers'
 
 // decimal and exponent
@@ -17,4 +16,11 @@ console.log(ethers.utils.formatBytes32String('dog')) // to hex
 console.log(ethers.utils.isHexString(ethers.utils.formatBytes32String('dog'))) // true
 console.log(ethers.utils.formatEther(ether)) // 1 ether, safe
 console.log(ethers.utils.formatEther(etherString)) // 1 ether, but not safe
+console.log('parsed: ', ethers.utils.parseEther('1').toString())
 console.log(ethers.utils.formatUnits(100000000000, 'ether'))
+
+const byteString = '0x6a616b652073756e670000000000000000000000000000000000000000000000'
+const stringString = 'jake sung'
+console.log(ethers.utils.parseBytes32String(byteString))
+console.log(ethers.utils.formatBytes32String(stringString))
+console.log(ethers.utils.parseBytes32String(byteString) === stringString ? 'same value: bytes32 and string' : 'conversion failed')
