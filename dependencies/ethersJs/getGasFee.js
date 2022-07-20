835ce9f5f33fc522cbe8eb57f35b51b4a4bf61ec
designerasun <asuntheawesome@gmail.com>
2022-07-21 01:19:11 +0900

    ENH:signer/wallet get gas price

--- dependencies/ethersJs/getGasFee.js
+++ dependencies/ethersJs/getGasFee.js
@@ -16,6 +16,19 @@ const defaultProvider = new ethers.getDefaultProvider('ropsten', {
 // individual provider
 const provider = new ethers.providers.InfuraProvider('ropsten')
 
+// signer
+const jakeSigner = new ethers.Wallet(process.env.ACCOUNT_ROPSTEN_PRIVATE_KEY, provider)
+console.log(jakeSigner.address)
+
+console.log(await jakeSigner.getGasPrice()) // hex
+console.log(parseInt(await jakeSigner.getGasPrice())) // number
+console.log((await jakeSigner.getGasPrice()).toString())
+
+const currentGasPrice = await jakeSigner.getGasPrice()
+console.log(ethers.utils.hexlify(parseInt(currentGasPrice)))
+
+console.log(ethers.utils.formatUnits(await jakeSigner.getGasPrice(), 'gwei'))
+
 // getGasPrice is a legacy
 console.log(BigNumber.from(await provider.getGasPrice()).toBigInt())
 
