434633172ee099f3d932755cad473b4fbe78e847
designerasun <asuntheawesome@gmail.com>
2022-07-29 00:59:52 +0900

    ENH: 0x swap sdk exercise

--- dependencies/ethersJs/swap.js
+++ dependencies/ethersJs/swap.js
@@ -1,6 +1,7 @@
 import { NftSwap } from '@traderxyz/nft-swap-sdk'
 import { ethers } from 'ethers'
 import dotenv from 'dotenv'
+import { NftSwapV4, NftSwapV3 } from '@traderxyz/nft-swap-sdk'
 
 dotenv.config()
 
@@ -12,8 +13,44 @@ const provider = new ethers.getDefaultProvider('ropsten', {
     projectSecret,
 })
 
-const privateKey = process.env.ACCOUNT_ROPSTEN_PRIVATE_KEY
+const chainId = 3
 
-const signer = new ethers.Wallet(privateKey, provider)
-const nftSwap = new NftSwap(provider, signer, ethers.providers.getNetwork())
-console.log(provider._network.chainId)
+// const nftSwapV3 = new NftSwapV3(provider, signerForMaker, chainId)
+
+const MY_NFT_A = {
+    tokenAddress: '',
+    tokenId: '0',
+    type: 'ERC721',
+}
+
+const WETH = {
+    tokenAddress: '',
+    amount: '',
+    type: 'ERC20',
+}
+
+const makerAddress = ''
+
+// maker: build order
+const privateKeyForMaker = process.env.ACCOUNT_ROPSTEN_PRIVATE_KEY
+const signerForMaker = new ethers.Wallet(privateKeyForMaker, provider)
+
+const nftSwapV4Maker = new NftSwapV4(provider, privateKeyForMaker, chainId)
+await nftSwapV4Maker.approveTokenOrNftByAsset(MY_NFT_A, makerAddress)
+
+const order = nftSwapV4Maker.buildOrder(MY_NFT_A, WETH, makerAddress)
+
+const signedOrder = await nftSwapV4Maker.signOrder(order)
+
+// taker:
+const privateKeyForTaker = process.env.TAKER_PRIVATE_KEY
+const signerForTaker = new ethers.Wallet(privateKeyForTaker, provider)
+const takerAddress = ''
+const nftSwapV4Taker = new NftSwapV4(provider, signerForTaker)
+
+await nftSwapV4Taker.approveTokenOrNftByAsset(WETH, signerForTaker)
+
+const fillTx = await nftSwapV4Taker.fillSignedOrder(signedOrder)
+const fillTxReceipt = await nftSwapV4Taker.awaitTransactionHash(fillTx.hash)
+
+console.log(fillTxReceipt.transactionHash)
